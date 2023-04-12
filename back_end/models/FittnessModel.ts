9c41d4d62bca53f72474c1fa2298f509dde165d5
Morning <morning@1-17.local>
2023-04-12 10:29:34 +0800

    added fitness schema

--- back_end/models/FittnessModel.ts
+++ back_end/models/FittnessModel.ts
@@ -1,7 +1,51 @@
 import mongoose from "mongoose";
 
-const fitnessSchema = new mongoose.Schema({}, { collection: "Fitness" });
+type Contact = {
+  phone_number: number;
+  email: string;
+};
 
-const Fitness = mongoose.model("Fitness", fitnessSchema);
+type Spec = {
+  wifi: boolean;
+  shower: boolean;
+  parking: boolean;
+};
+
+const fitnessSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: [String],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: false,
+    },
+    address: {
+      district: String,
+      description: String,
+      coordinates: {
+        latitude: Number,
+        longtitude: Number,
+      },
+    },
+
+    contact: {
+      phone_number: Number,
+      email: String,
+    },
+  },
+  { collection: "Fitness" },
+);
+
+const Fitness = mongoose.model("Fitness", fitnessSchema, "Fitness");
 
 export default Fitness;
