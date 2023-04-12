bc243523e6090b26329566ba2f9a07a932227843
Morning <morning@1-17.local>
2023-04-12 11:44:02 +0800

    fitness schema added by MNation

--- back_end/models/FittnessModel.ts
+++ back_end/models/FittnessModel.ts
@@ -1,51 +1,98 @@
 import mongoose from "mongoose";
 
-type Contact = {
-  phone_number: number;
-  email: string;
-};
+interface Fitness {
+  _id: string;
+  name: string;
+  description: string;
+  image: Array<string>;
+  price: {
+    oneMonth: number;
+    oneTime?: number;
+    threeMonth?: number;
+    sixMonth?: number;
+    year?: number;
+  };
+  contact: {
+    phone_number: number;
+    email?: string;
+    social?: Array<string>;
+  };
+  address: {
+    district:
+      | "Khan uul"
+      | "Nalaikh"
+      | "Songino Khairkhan"
+      | "Sukhbaatar"
+      | "Chilgeltei"
+      | "Bayanzurkh"
+      | "Bayangol"
+      | "Baganuur";
+    description: string;
+    coord: Array<number>;
+  };
+  timetable: {
+    monday: string;
+    tuesday: string;
+    wednesday: string;
+    thursday: string;
+    friday: string;
+    saturday: string;
+    sunday: string;
+  };
+  discount?: [{ name: string; discount: number }];
 
-type Spec = {
-  wifi: boolean;
-  shower: boolean;
-  parking: boolean;
-};
+  spec?: {
+    wifi: boolean;
+    shower: boolean;
+    parking: boolean;
+  };
+}
 
-const fitnessSchema = new mongoose.Schema(
+const fitnessSchema = new mongoose.Schema<Fitness>(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: [String],
-      required: true,
-    },
+    name: String,
+    description: String,
+    image: [String],
     price: {
-      type: Number,
-      required: false,
+      oneMonth: Number,
+      oneTime: Number,
+      threeMonth: Number,
+      sixMonth: Number,
+      year: Number,
+    },
+    contact: {
+      phone_number: Number,
+      email: String,
+      social: [String],
     },
     address: {
       district: String,
       description: String,
-      coordinates: {
-        latitude: Number,
-        longtitude: Number,
+      coord: {
+        Type: { type: String, default: "Pointer" },
+        coordinates: [Number],
       },
     },
-
-    contact: {
-      phone_number: Number,
-      email: String,
+    timetable: {
+      monday: String,
+      tuesday: String,
+      wednesday: String,
+      thursday: String,
+      friday: String,
+      saturday: String,
+      sunday: String,
+    },
+    discount: [{ name: String, discount: Number }],
+    spec: {
+      wifi: Boolean,
+      shower: Boolean,
+      parking: Boolean,
     },
   },
   { collection: "Fitness" },
 );
 
 const Fitness = mongoose.model("Fitness", fitnessSchema, "Fitness");
+Fitness.collection.createIndex({ "address.coord": "2dsphere" });
 
 export default Fitness;
