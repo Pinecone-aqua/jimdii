ff64a58c763cc922ece3bee4342f12b02d3df5b2
Morning <morning@admins-iMac-1-8.local>
2023-04-13 12:32:02 +0800

    unfinished login & register

--- front_end/client/pages/Login.tsx
+++ front_end/client/pages/Login.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [registerbtn, setRegisterBtn] = useState<boolean>(false);
+  const btnStyle = "m-3 w-3/6 bg-white text-black";
+
   return (
-    <div>
-      <div>
-        <form action="">
-          <p>Login</p>
-        </form>
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-[300px] h-[400px] border-2 p-2">
+        <div className="w-full flex p-5">
+          <button className={btnStyle} onClick={() => setRegisterBtn(false)}>
+            Login
+          </button>
+          <button className={btnStyle} onClick={() => setRegisterBtn(true)}>
+            Register
+          </button>
+        </div>
+        {!registerbtn ? (
+          <form action="" className="flex flex-col gap-2">
+            <p>Login</p>
+            <input type="text" />
+            <input type="text" />
+            <button>login</button>
+          </form>
+        ) : (
+          <div>register</div>
+        )}
       </div>
     </div>
   );
