40322e11ceb82205aa231d2713c2ccd7de70aed1
Morning <morning@1-17.local>
2023-04-21 11:21:57 +0800

    adding fitness crud unfinished

---
+++ backend/src/fitness/fitness.model.ts
@@ -0,0 +1,53 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Fitness {
+  @Prop({ type: String })
+  name: string;
+  @Prop({ type: String })
+  description: string;
+  @Prop({ type: Array })
+  image: string[];
+  @Prop({ type: Object })
+  price: {
+    oneMonth: number;
+    threeMonth: number;
+    onetime: number;
+    year: number;
+  };
+  @Prop({ type: Object })
+  address: {
+    district: string;
+    description: string;
+    coord: number[];
+  };
+  @Prop({ type: Object })
+  timetable: {
+    Monday: string;
+    Tuesday: string;
+    Wednesday: string;
+    Thursday: string;
+    Friday: string;
+    Saturday: string;
+    Sunday: string;
+  };
+  @Prop({ type: Array })
+  discount: [
+    { name: string; discount: number },
+    { name: string; discount: number },
+    { name: string; discount: number },
+  ];
+  @Prop({ type: Object })
+  spec: {
+    wifi: boolean;
+    shower: boolean;
+    parking: boolean;
+  };
+  @Prop({ type: Object })
+  contact: {
+    phone_number: number;
+    social: string[];
+  };
+}
+
+export const FitnessSchema = SchemaFactory.createForClass(Fitness);
