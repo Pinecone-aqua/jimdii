440aa21ffd29ef1af749e7f75cb9274cd6182c70
unknown <bataa.banaa@gmail.com>
2023-04-25 07:07:07 +0800

    add fit un

--- front_end/dashboard/components/AddFitness.tsx
+++ front_end/dashboard/components/AddFitness.tsx
@@ -1,3 +1,19 @@
-export default function AddFitness() {
-  return <>new</>;
+import { Dispatch, SetStateAction } from "react";
+
+interface PropType {
+	setAddFitness: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function AddFitness(prop: PropType) {
+	const { setAddFitness } = prop;
+	return (
+		<div className="w-screen h-screen top-0 left-0 bg-gray-300/50 absolute flex justify-center items-center">
+			<div
+				className="w-full h-full"
+				onClick={() => setAddFitness(false)}>
+				{" "}
+			</div>
+			<div className="w-10/12 h-5/6 bg-white absolute">hi</div>
+		</div>
+	);
 }
--- front_end/dashboard/pages/fitness.tsx
+++ front_end/dashboard/pages/fitness.tsx
@@ -1,48 +1,59 @@
 import { FiSearch, FiPlus } from "react-icons/fi";
 import { fitnesses } from "../utils/fitness.dummy";
 import FitnessCol from "@/components/FitnessCol";
+import { useState } from "react";
+import AddFitness from "@/components/AddFitness";
 
 export default function Fitness() {
-  return (
-    <>
-      <div className="bg-white h-full">
-        <div className="w-full flex align-center justify-between px-6 py-4">
-          <form className="flex w-1/2 ">
-            <label className="relative flex items-center w-3/4 text-red-950">
-              <FiSearch className="absolute m-2" />
-              <input
-                className="placeholder:text-red-950 block bg-slate-200 w-full border-b-2 border-red-950  rounded-l-2xl py-2 pl-9 pr-3 focus:outline-0"
-                placeholder="Search for anything..."
-                type="text"
-                name="search"
-              />
-            </label>
-            <button className="bg-red-950 px-4 rounded-r-2xl text-white">
-              Search
-            </button>
-          </form>
-          <button className="bg-red-950 px-4 rounded-2xl text-white flex items-center">
-            <FiPlus /> Add Fitness
-          </button>
-        </div>
-        <div>
-          <table className="table-auto text-left w-full border-2 ">
-            <thead>
-              <tr className="h-20">
-                <th scope="col">No</th>
-                <th scope="col">Fitness Name</th>
-                <th scope="col">Members</th>
-                <th scope="col">Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {fitnesses.map((fitness, i) => (
-                <FitnessCol fitness={fitness} i={i} key={i} />
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </>
-  );
+	const [addFitness, setAddFitness] = useState(false);
+
+	return (
+		<>
+			<div className="bg-white h-full">
+				<div className="w-full flex align-center justify-between px-6 py-4">
+					<form className="flex w-1/2 ">
+						<label className="relative flex items-center w-3/4 text-red-950">
+							<FiSearch className="absolute m-2" />
+							<input
+								className="placeholder:text-red-950 block bg-slate-200 w-full border-b-2 border-red-950  rounded-l-2xl py-2 pl-9 pr-3 focus:outline-0"
+								placeholder="Search for anything..."
+								type="text"
+								name="search"
+							/>
+						</label>
+						<button className="bg-red-950 px-4 rounded-r-2xl text-white">
+							Search
+						</button>
+					</form>
+					<button
+						className="bg-red-950 px-4 rounded-2xl text-white flex items-center"
+						onClick={() => setAddFitness(true)}>
+						<FiPlus /> Add Fitness
+					</button>
+					{addFitness && <AddFitness setAddFitness={setAddFitness} />}
+				</div>
+				<div>
+					<table className="table-auto text-left w-full border-2 ">
+						<thead>
+							<tr className="h-20">
+								<th scope="col">No</th>
+								<th scope="col">Fitness Name</th>
+								<th scope="col">Members</th>
+								<th scope="col">Delete</th>
+							</tr>
+						</thead>
+						<tbody>
+							{fitnesses.map((fitness, i) => (
+								<FitnessCol
+									fitness={fitness}
+									i={i}
+									key={i}
+								/>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</>
+	);
 }
