c5d417d5a670aafec93500d4a5fb387e2d174de2
Morning <morning@admins-iMac-1-8.local>
2023-04-26 12:44:21 +0800

    multi file upload

--- front_end/dashboard/components/AddFitness.tsx
+++ front_end/dashboard/components/AddFitness.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Dispatch, SetStateAction, useState } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 
 interface PropType {
   setAddFitness: Dispatch<SetStateAction<boolean>>;
@@ -7,16 +7,30 @@ interface PropType {
 
 export default function AddFitness(prop: PropType) {
   const { setAddFitness } = prop;
-  const [files, setFiles] = useState();
+  const [files, setFiles] = useState<File[] | null>(null);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   function submitHandler(e: any) {
     e.preventDefault();
+    // const imageArr = [];
+    if (e.target.file.files) {
+      const imageFrom = new FormData();
+      const files: File[] = e.target.file.files;
+      Array.from(files).forEach((file) => imageFrom.append("image", file));
 
-    console.log(e.target.file.files[0]);
-    const imageFrom = new FormData();
-    imageFrom.append("file", e.target.file.files[0]);
-    axios.post("http://localhost:7003/fitness/test", imageFrom);
+      // imageFrom.append("name", e.target.title.value);
+      console.log(e.target.file.files);
+      axios
+        .post("http://localhost:7003/fitness/test", imageFrom)
+        .then((res) => console.log(res));
+    }
+  }
+
+  function showImages(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files) {
+      const files: FileList = e.target.files;
+      setFiles(Object.values(files));
+    }
   }
 
   return (
@@ -25,18 +39,9 @@ export default function AddFitness(prop: PropType) {
       <div className="w-10/12 h-5/6 bg-white absolute">
         <form className="p-5" onSubmit={submitHandler}>
           <>
-            <input
-              type="file"
-              name="file"
-              multiple
-              onChange={(e) =>
-                e.target.files &&
-                typeof e.target.files === "object" &&
-                setFiles(Object.values(e.target.files))
-              }
-            />
+            <input type="file" name="file" multiple onChange={showImages} />
             <div className="overflow-x-scroll snap-x flex">
-              {files?.map((file, i) => (
+              {files?.map((file: File, i: number) => (
                 <picture key={i} className="m-10 snap-center">
                   <img
                     src={URL.createObjectURL(file)}
@@ -48,7 +53,9 @@ export default function AddFitness(prop: PropType) {
               ))}
             </div>
           </>
-          <div className=" bg-black">a</div>
+          <div>
+            <input type="text" className="bg-gray-300" name="title" />
+          </div>
           <button type="submit">submit</button>
         </form>
       </div>
