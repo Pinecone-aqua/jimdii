87c26fddf1c10309b1c58a4adae36c72d6bb1341
Murunation <murunbattulga6@gmail.com>
2023-04-29 18:19:28 +0800

    working on register type

--- front_end/client/util/types.ts
+++ front_end/client/util/types.ts
@@ -13,3 +13,50 @@ export interface UserType {
   fitness_id?: string;
   birth_date?: string;
 }
+
+export interface FitnessType {
+  name: string;
+  description: string;
+  image: string[];
+  price: {
+    oneMonth: number;
+    threeMonth?: number;
+    onetime?: number;
+    year?: number;
+  };
+  address: {
+    district:
+      | 'Bayangol'
+      | 'Sukhbaatar'
+      | 'Khan-uul'
+      | 'Songino-Khairkhan'
+      | 'Bayanzurkh'
+      | 'Chingeltei'
+      | 'Baganuur';
+    description: string;
+    coord?: number[];
+  };
+  timetable: {
+    Monday: string;
+    Tuesday: string;
+    Wednesday: string;
+    Thursday: string;
+    Friday: string;
+    Saturday: string;
+    Sunday: string;
+  };
+  discount?: [
+    { name: string; discount: number },
+    { name: string; discount: number },
+    { name: string; discount: number },
+  ];
+  spec: {
+    wifi: boolean;
+    shower: boolean;
+    parking: boolean;
+  };
+  contact: {
+    phone_number: number;
+    social?: string[];
+  };
+}
