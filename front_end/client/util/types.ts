2cd5016002ae931cd300b8842cdbd8e674adb40e
Murunation <murunbattulga6@gmail.com>
2023-04-29 18:19:28 +0800

    working on register type

--- front_end/client/util/types.ts
+++ front_end/client/util/types.ts
@@ -1,85 +1,132 @@
 export interface UserType {
-	username: {
-		firstname: string;
-		lastname: string;
-	};
-	phone?: number;
-	email: string;
-	password: string;
-	address?: {
-		coord: number[];
-	};
-	gender?: "male" | "female" | "other";
-	fitness_id?: string;
-	birth_date?: string;
+  username: {
+    firstname: string;
+    lastname: string;
+  };
+  phone?: number;
+  email: string;
+  password: string;
+  address?: {
+    coord: number[];
+  };
+  gender?: "male" | "female" | "other";
+  fitness_id?: string;
+  birth_date?: string;
 }
 
 export interface FitnessType {
-	_id?: string;
-	name: string;
-	description?: string;
-	image: string[];
-	price?: {
-		oneMonth: number;
-		threeMonth?: number;
-		onetime?: number;
-		year?: number;
-	};
-	address?: {
-		district:
-			| "Bayangol"
-			| "Sukhbaatar"
-			| "Khan-uul"
-			| "Songino-Khairkhan"
-			| "Bayanzurkh"
-			| "Chingeltei"
-			| "Baganuur";
-		description: string;
-		coord?: number[];
-	};
-	timetable?: TimetableType;
-	discount?: DiscountType[];
-	spec?: {
-		wifi: boolean;
-		shower: boolean;
-		parking: boolean;
-	};
-	contact?: ContactType;
+  _id?: string;
+  name: string;
+  description?: string;
+  image: string[];
+  price?: {
+    oneMonth: number;
+    threeMonth?: number;
+    onetime?: number;
+    year?: number;
+  };
+  address?: {
+    district:
+      | "Bayangol"
+      | "Sukhbaatar"
+      | "Khan-uul"
+      | "Songino-Khairkhan"
+      | "Bayanzurkh"
+      | "Chingeltei"
+      | "Baganuur";
+    description: string;
+    coord?: number[];
+  };
+  timetable?: TimetableType;
+  discount?: DiscountType[];
+  spec?: {
+    wifi: boolean;
+    shower: boolean;
+    parking: boolean;
+  };
+  contact?: ContactType;
 }
 export interface TimetableType {
-	Monday: string;
-	Tuesday: string;
-	Wednesday: string;
-	Thursday: string;
-	Friday: string;
-	Saturday: string;
-	Sunday: string;
+  Monday: string;
+  Tuesday: string;
+  Wednesday: string;
+  Thursday: string;
+  Friday: string;
+  Saturday: string;
+  Sunday: string;
 }
 
 export interface PriceType {
-	onetime?: number;
-	oneMonth: number;
-	threeMonth?: number;
-	sixMonth?: number;
-	year?: number;
+  onetime?: number;
+  oneMonth: number;
+  threeMonth?: number;
+  sixMonth?: number;
+  year?: number;
 }
 
 export interface DiscountType {
-	name: string;
-	discount: number;
+  name: string;
+  discount: number;
 }
 
 export interface ContactType {
-	phonenumber?: number;
-	social?: string[];
+  phonenumber?: number;
+  social?: string[];
 }
 
 export interface FitnessProp {
-	data: FitnessType;
+  data: FitnessType;
 }
 
 export interface AllFitnessProp {
-	data: FitnessType[];
+  data: FitnessType[];
 }
 
 // export interface SpecType {}
+
+export interface FitnessType {
+  name: string;
+  description: string;
+  image: string[];
+  price: {
+    oneMonth: number;
+    threeMonth?: number;
+    onetime?: number;
+    year?: number;
+  };
+  address: {
+    district:
+      | "Bayangol"
+      | "Sukhbaatar"
+      | "Khan-uul"
+      | "Songino-Khairkhan"
+      | "Bayanzurkh"
+      | "Chingeltei"
+      | "Baganuur";
+    description: string;
+    coord?: number[];
+  };
+  timetable: {
+    Monday: string;
+    Tuesday: string;
+    Wednesday: string;
+    Thursday: string;
+    Friday: string;
+    Saturday: string;
+    Sunday: string;
+  };
+  discount?: [
+    { name: string; discount: number },
+    { name: string; discount: number },
+    { name: string; discount: number }
+  ];
+  spec: {
+    wifi: boolean;
+    shower: boolean;
+    parking: boolean;
+  };
+  contact: {
+    phone_number: number;
+    social?: string[];
+  };
+}
