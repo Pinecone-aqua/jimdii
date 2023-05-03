7a11fa059ed52f33e7bc85c5309993931658d544
Morning <morning@admins-iMac-1-8.local>
2023-05-03 12:30:54 +0800

    pagination

---
+++ front_end/client/pages/test.tsx
@@ -0,0 +1,11 @@
+import Pagination from "@/components/Pagination";
+
+export default function Test() {
+  return (
+    <div className="min-h-screen text-white bg-gray-900">
+      <div className="container flex justify-center items-center">
+        <Pagination />
+      </div>
+    </div>
+  );
+}
