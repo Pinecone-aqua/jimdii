9cfc35d17a98ef3ecabf332777d6c52b2c8f56bf
unknown <bataa.banaa@gmail.com>
2023-05-07 22:11:42 +0800

    profile button

---
+++ front_end/client/components/profile/UserDetail.tsx
@@ -0,0 +1,20 @@
+export default function UserDetail() {
+	return (
+		<div>
+			<form action="">
+				<input
+					type="text"
+					placeholder="username"
+				/>
+				<input
+					type="text"
+					placeholder="username"
+				/>
+				<input
+					type="text"
+					placeholder="username"
+				/>
+			</form>
+		</div>
+	);
+}
