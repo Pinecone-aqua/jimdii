bf40a065787c8dfc49ffde476a64ac811f961dca
Morning <morning@admins-iMac-1-8.local>
2023-05-08 12:49:25 +0800

    unfinished

--- front_end/client/util/types.ts
+++ front_end/client/util/types.ts
@@ -1,11 +1,9 @@
 export interface UserType {
-  username: {
-    firstname: string;
-    lastname: string;
-  };
-  phone?: number;
+  name: string;
   email: string;
-  password: string;
+  image?: string;
+  phone?: number;
+  password?: string;
   address?: {
     coord: number[];
   };
@@ -80,53 +78,4 @@ export interface FitnessProp {
 
 export interface AllFitnessProp {
   data: FitnessType[];
-}
-
-// export interface SpecType {}
-
-export interface FitnessType {
-  name: string;
-  description: string;
-  image: string[];
-  price: {
-    oneMonth: number;
-    threeMonth?: number;
-    onetime?: number;
-    year?: number;
-  };
-  address: {
-    district:
-      | "Bayangol"
-      | "Sukhbaatar"
-      | "Khan-uul"
-      | "Songino-Khairkhan"
-      | "Bayanzurkh"
-      | "Chingeltei"
-      | "Baganuur";
-    description: string;
-    coord?: number[];
-  };
-  timetable: {
-    Monday: string;
-    Tuesday: string;
-    Wednesday: string;
-    Thursday: string;
-    Friday: string;
-    Saturday: string;
-    Sunday: string;
-  };
-  discount?: [
-    { name: string; discount: number },
-    { name: string; discount: number },
-    { name: string; discount: number }
-  ];
-  spec: {
-    wifi: boolean;
-    shower: boolean;
-    parking: boolean;
-  };
-  contact: {
-    phone_number: number;
-    social?: string[];
-  };
 }
