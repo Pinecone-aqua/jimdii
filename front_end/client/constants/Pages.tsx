b4e9ba7f9868d4ee36d280cfa509327d89f0c3b1
Batsu28 <bataa.banaa@gmail.com>
2023-05-09 12:30:33 +0800

    profile membership card

--- front_end/client/constants/Pages.tsx
+++ front_end/client/constants/Pages.tsx
@@ -4,19 +4,19 @@ import UserDetail from "@/components/profile/UserDetail";
 import { BsCardList, BsClockHistory, BsPersonCircle } from "react-icons/bs";
 
 export const ProfileNavbar = [
-	{
-		name: "Profile",
-		component: <UserDetail />,
-		icon: <BsPersonCircle />,
-	},
-	{
-		name: "Membership",
-		component: <Membership />,
-		icon: <BsCardList />,
-	},
-	{
-		name: "History",
-		component: <History />,
-		icon: <BsClockHistory />,
-	},
+  {
+    name: "Profile",
+    component: <UserDetail />,
+    icon: <BsPersonCircle />,
+  },
+  {
+    name: "Membership",
+    component: <Membership />,
+    icon: <BsCardList />,
+  },
+  {
+    name: "History",
+    component: <History />,
+    icon: <BsClockHistory />,
+  },
 ];
