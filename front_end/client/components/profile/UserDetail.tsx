51ab5cf0ec7c1c016df29df5f71dc3a1c5dfc1ef
Batsu28 <bataa.banaa@gmail.com>
2023-05-11 13:37:55 +0800

    edit

--- front_end/client/components/profile/UserDetail.tsx
+++ front_end/client/components/profile/UserDetail.tsx
@@ -3,7 +3,17 @@ import { useState } from "react";
 
 export default function UserDetail() {
   const [editUserDetail, setEditUserDetail] = useState(false);
+  const [editedDetail, setEditedDetail] = useState({});
   const { currentUser } = useUser();
+
+  const inputStyle = `focus:outline-main mx-2 p-2 bg-${
+    editUserDetail ? "white" : "black"
+  } text-${editUserDetail ? "black" : "white"} min-w-[200px] caret-main`;
+  const labelStyle =
+    "flex flex-col md:flex-row md:items-center gap-2 md:w-1/2 lg:w-1/4 md:justify-between";
+  const labelTextStyle = "w-1/2";
+
+  console.log(currentUser);
 
   function editHandler() {
     if (!editUserDetail) {
@@ -25,25 +35,54 @@ export default function UserDetail() {
   }
 
   return (
-    <div className="p-5">
-      <form onSubmit={submitHandler} className="flex flex-col gap-2 my-2">
+    <div className="p-1 md:p-5">
+      <div onSubmit={submitHandler} className="flex flex-col gap-4 my-2">
         <button
           onClick={editHandler}
           className="py-2 px-4 border rounded-lg active:opacity-50 w-32"
         >
           {editUserDetail ? "Save" : "Edit"}
         </button>
-        <input
-          type="text"
-          className={`focus:outline-0 p-2 bg-${
-            editUserDetail ? "white" : "black"
-          } text-${editUserDetail ? "black" : "white"}`}
-          readOnly={editUserDetail ? false : true}
-          defaultValue={currentUser?.name}
-          name="username"
-          onChange={() => console.log("test")}
-        />
-      </form>
+        <label className={labelStyle}>
+          <p className={labelTextStyle}>Username:</p>
+          <input
+            type="text"
+            className={inputStyle}
+            readOnly={editUserDetail ? false : true}
+            defaultValue={currentUser?.name}
+            name="username"
+            onChange={(e) =>
+              setEditedDetail({ ...editedDetail, name: e.target.value })
+            }
+          />
+        </label>
+        <label className={labelStyle}>
+          <p className={labelTextStyle}>Email:</p>
+          <input
+            type="text"
+            className={inputStyle}
+            readOnly={editUserDetail ? false : true}
+            defaultValue={currentUser?.email}
+            name="birthdate"
+            onChange={(e) =>
+              setEditedDetail({ ...editedDetail, email: e.target.value })
+            }
+          />
+        </label>
+        <label className={labelStyle}>
+          <p className={labelTextStyle}>Birthdate:</p>
+          <input
+            type="date"
+            className={inputStyle}
+            readOnly={editUserDetail ? false : true}
+            defaultValue={currentUser?.birth_date}
+            name="birthdate"
+            onChange={(e) =>
+              setEditedDetail({ ...editedDetail, birthDate: e.target.value })
+            }
+          />
+        </label>
+      </div>
     </div>
   );
 }
