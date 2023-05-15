6f810be7a7e20b6348092f743232ee82b0dee825
Batsu28 <bataa.banaa@gmail.com>
2023-05-15 11:21:20 +0800

    custom hook test

--- front_end/dashboard/context/UserContext.tsx
+++ front_end/dashboard/context/UserContext.tsx
@@ -1,47 +1,45 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import { createContext, useContext } from "react";
 
 import { ReactNode } from "react";
 
 type PropType = {
-	children: ReactNode;
+  children: ReactNode;
 };
 
 type ContextType = {
-	loginHandler: () => void;
-	logoutHandler: () => void;
+  loginHandler: () => void;
+  logoutHandler: () => void;
 };
 
 const UserContext = createContext<ContextType>({} as ContextType);
 
 export default function UserProvider({ children }: PropType) {
-	const router = useRouter();
+  const router = useRouter();
 
-	function loginHandler() {
-		try {
-			axios
-				.get("http://localhost:7003/google/login")
-				.then((res) => {
-					router.push(res.data);
-				})
-				.catch((err) => console.log(err));
-		} catch (err) {
-			console.log(err);
-		}
-	}
+  function loginHandler() {
+    try {
+      axios.get("http://localhost:7003/google-login").then((res) => {
+        router.push(res.data);
+      });
+    } catch (err) {
+      loginHandler();
+    }
+  }
 
-	function logoutHandler() {
-		localStorage.removeItem("currentUser");
-	}
+  function logoutHandler() {
+    Cookies.remove("token");
+  }
 
-	return (
-		<UserContext.Provider value={{ loginHandler, logoutHandler }}>
-			{children}
-		</UserContext.Provider>
-	);
+  return (
+    <UserContext.Provider value={{ loginHandler, logoutHandler }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
 
 export function useUser() {
-	return useContext(UserContext);
+  return useContext(UserContext);
 }
