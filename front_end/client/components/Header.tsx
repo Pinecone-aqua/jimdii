c54a7736a6710969f70123b1378175e6743c90a5
Jamie <ochirjamia@gmail.com>
2023-05-15 13:42:09 +0800

    may15

--- front_end/client/components/Header.tsx
+++ front_end/client/components/Header.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import LoginLogo from "./subcomp/LoginLogo";
 import { Dropdown } from "flowbite-react";
 import MainResLogo from "./subcomp/MainResLogo";
 import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 
 interface HeaderType {
   user: any;
@@ -13,12 +14,33 @@ interface HeaderType {
 }
 
 export default function Header({ user, setUser }: HeaderType): JSX.Element {
-  user && console.log(user);
+  const [scrollY, setScrollY] = useState(0);
+  const router = useRouter();
+
+  const onScroll = useCallback(() => {
+    // console.log(window);
+    // const { pageYOffset, scrollY } = window;
+    // console.log("yOffset", pageYOffset, "scrollY", scrollY);
+    setScrollY(window.pageYOffset);
+  }, []);
+
+  useEffect(() => {
+    //add eventlistener to window
+    window.addEventListener("scroll", onScroll, { passive: true });
+    // remove event on unmount to prevent a memory leak with the cleanup
+    return () => {
+      window.removeEventListener("scroll", onScroll, { passive: true });
+    };
+  }, []);
 
   return (
-    <header className="bg-none sticky top-0 z-20 h-[10vh] flex items-center">
-      <div className="container mx-auto  flex items-center justify-between">
-        <Dropdown
+    <header
+      className={`${
+        scrollY < 60 && router.asPath == "/" ? "bg-none" : "bg-black z-40"
+      } sticky top-0 z-20 h-[120px] flex items-center transition ease-in text-main whitespace-nowrap`}
+    >
+      <div className="flex container mx-auto justify-between items-center">
+        {/* <Dropdown
           label="//"
           className="bg-transparent text-white"
           id="headerDropdown"
@@ -41,78 +63,56 @@ export default function Header({ user, setUser }: HeaderType): JSX.Element {
             </Link>
           </Dropdown.Item>
           <Dropdown.Item>
-            <Link href={`/map`} className="hover:text-[#4D9799]">
-              Газрын зураг
-            </Link>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Link href={`/membership`} className="hover:text-[#4D9799]">
-              Гишүүнчлэл
-            </Link>
-          </Dropdown.Item>
-          <Dropdown.Item>
             <Link href={`/signUpGym`} className="hover:text-[#4D9799]">
               Gym-ээ бүртгүүлэх
             </Link>
           </Dropdown.Item>
 
           <Dropdown.Divider />
-        </Dropdown>
-        <Link href={`/`}>
-          <MainResLogo />
-        </Link>
-        <nav className="hidden lg:flex  w-1/2 justify-between   text-white">
-          <div className="w-[70%] flex gap-[30px]">
-            <Link href={`/`} className="hover:text-[#4D9799]">
+        </Dropdown> */}
+        <div className="w-1/5">
+          <Link href={`/`}>
+            <MainResLogo />
+          </Link>
+        </div>
+        <div className="w-1/2">
+          <nav className="text-white">
+            <Link href={`/`} className="hover:text-main">
               Нүүр хуудас
             </Link>
-            <Link href={`/allFitness/1`} className="hover:text-[#4D9799]">
+            <Link href={`/allFitness/1`} className="hover:text-main">
               Жийм
             </Link>
-            <Link href={`/map`} className="hover:text-[#4D9799]">
-              Газрын зураг
-            </Link>
-            <Link href={`/membership`} className="hover:text-[#4D9799]">
-              Гишүүнчлэл
-            </Link>
-          </div>
-        </nav>
-        <div className="w-1/4 flex justify-between items-center">
-          {/* <Link href={`/login`} className="flex items-center">
-            <div className="flex justify-end gap-[5px] text-[#4D9799] w-[80px] md:w-[90px] m-0">
-              <LoginLogo />
-              <p className="text-sm">Log In</p>
-            </div>
-          </Link> */}
+          </nav>
+        </div>
+
+        <div className="">
           {user ? (
-            <div className="flex items-center w-[200px] justify-between">
-              <Link
-                href={`/#`}
-                className="flex w-1/2 items-center justify-between hidden sm:inline"
+            <div className="flex items-center">
+              <div className="flex items-center gap-5">
+                <picture className="">
+                  <img
+                    className="w-[30px] rounded-[50%]"
+                    src={user.image}
+                    alt=""
+                  />
+                </picture>
+                <p className="text-md ">{user.name}</p>
+              </div>
+              <div
+                className="cursor-pointer text-main text-md"
+                onClick={() => {
+                  Cookies.remove("token");
+                  setUser("");
+                }}
               >
-                <div className="flex justify-end gap-[5px] text-[#4D9799] w-[80px] md:w-[90px] m-0">
-                  <picture className="flex items-center">
-                    <img className="w-[100px]" src={user.image} alt="" />
-                  </picture>
-                  <p className="text-sm ">{user.name}</p>
-                </div>
-              </Link>
-              <div>
-                <div
-                  className="cursor-pointer text-main"
-                  onClick={() => {
-                    Cookies.remove("token");
-                    setUser("");
-                  }}
-                >
-                  LOG OUT
-                </div>
+                Log Out
               </div>
             </div>
           ) : (
-            <div className="flex gap-5">
+            <div className="">
               <Link href={`/login`} className="flex items-center">
-                <div className="flex justify-end gap-[5px] text-[#4D9799] w-[80px] md:w-[90px] m-0">
+                <div className="">
                   <LoginLogo />
                   <p className="text-sm">Log In</p>
                 </div>
