05549c2ac7b90e3dee0667da6b7a2f88b71ef042
Batsu28 <bataa.banaa@gmail.com>
2023-05-16 11:26:54 +0800

    admin dashboard google login

--- front_end/dashboard/context/UserContext.tsx
+++ front_end/dashboard/context/UserContext.tsx
@@ -1,6 +1,8 @@
-import { UserType } from "@/util/types";
+import { UserType } from "@/utils/types";
+import axios from "axios";
 import Cookies from "js-cookie";
 import jwtDecode from "jwt-decode";
+import { useRouter } from "next/router";
 import React, {
   ReactNode,
   createContext,
@@ -11,6 +13,7 @@ import React, {
 
 interface UserContextType {
   currentUser: UserType | null;
+  logoutHandler: () => void;
 }
 
 const UserContext = createContext<UserContextType>({} as UserContextType);
@@ -21,20 +24,41 @@ export function useUser() {
 
 interface UserProviderType {
   children: ReactNode;
+  children: ReactNode;
 }
 
 export default function UserProvider({ children }: UserProviderType) {
   const [currentUser, setCurrentUser] = useState<UserType | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
-    const token = Cookies.get("token");
+    const token = Cookies.get("aToken");
+    // if (!token && router.asPath !== "/login") {
+    //   router.push("/login");
+    //   return;
+    // }
     if (token) {
-      setCurrentUser(jwtDecode(token));
+      try {
+        axios
+          .get("http://localhost:7003/user/checkToken?role=admin", {
+            headers: { Authorization: token },
+          })
+          .then(() => setCurrentUser(jwtDecode(token)))
+          .catch(() => (Cookies.remove("atoken"), setCurrentUser(null)));
+      } catch (err) {
+        console.log(err);
+      }
     }
-  }, []);
+  }, [router]);
+
+  function logoutHandler() {
+    Cookies.remove("aToken");
+    setCurrentUser(null);
+    router.push("/login");
+  }
 
   return (
-    <UserContext.Provider value={{ currentUser }}>
+    <UserContext.Provider value={{ currentUser, logoutHandler }}>
       {children}
     </UserContext.Provider>
   );
