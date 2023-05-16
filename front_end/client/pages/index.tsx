0413f5ad324580e06b2bb97a3501540627680d53
wick <ochirjamia@gmail.com>
2023-05-16 21:00:07 +0800

    may16

--- front_end/client/pages/index.tsx
+++ front_end/client/pages/index.tsx
@@ -10,9 +10,10 @@ import Logo from "@/components/subcomp/Logo";
 import Service from "@/components/subcomp/Service";
 import { FitnessType } from "@/util/types";
 import axios from "axios";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
-import { useParallax } from "react-scroll-parallax";
+import SmoothScrollLink from "@/components/subcomp/SmoothScrollLink";
+// import { useParallax } from "react-scroll-parallax";
 // import "../styles/index.css";
 
 // import {
@@ -54,43 +55,63 @@ export default function Home(props: PropsType) {
 
   return (
     <main className="text-white">
-      <div className="relative z-30 py-40 pl-60">
-        <Logo />
-        <p className="text-3xl">Монголын анхны цахим фитнесс захиалга</p>
-        <div className="py-20 flex gap-5">
-          <button className="secondaryButton">Үйлчилгээний тухай</button>
+      <div className="relative z-30 py-20 px-16 lg:py-40 lg:pl-60">
+        <div className="w-[200px] h-[50px] ">
+          <Logo />
+        </div>
+
+        <p className="text-sm lg:text-3xl">
+          Монголын анхны цахим фитнесс захиалга
+        </p>
+        <div className="h-[80px] py-5 lg:py-20 flex items-center gap-5">
+          <SmoothScrollLink to="service">
+            <button className="secondaryButton">Үйлчилгээний тухай</button>
+          </SmoothScrollLink>
           <Link href={`/allFitness/1`}>
             <button className="mainButton">Захиалах</button>
           </Link>
         </div>
       </div>
-
-      <div className="relative">
-        <div id="pattern">
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            // id="pattern"
-            className="rope"
-          >
-            <source
-              src={scrollY < 700 ? `./rope.mp4` : `./running.mp4`}
-              type="video/mp4"
-            />
-          </video>
+      {scrollY < 850 && (
+        <div className="relative">
+          <div id="pattern">
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              // id="pattern"
+              className="rope"
+            >
+              <source src="./rope.mp4" type="video/mp4" />
+            </video>
+          </div>
         </div>
-      </div>
-
-      <section className="h-[80px] md:h-[120px] lg:h-[160px] flex items-center text-center bg-main w-full text-sm  sm:text-2xl  lg:text-4xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#90FFFF] via-main to-main">
+      )}{" "}
+      {scrollY > 850 && (
+        <div className="relative">
+          <div id="pattern">
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              // id="pattern"
+              className="rope"
+            >
+              <source src="./running.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      )}
+      <section className="h-[80px] flex items-center text-center bg-main w-full text-sm md:h-[120px] lg:h-[160px]   sm:text-2xl  lg:text-4xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#90FFFF] via-main to-main">
         <p className="container mx-auto w-3/4 text-white ">
           "Эхлэх хамгийн зөв цаг нь өчигдөр байсан. Харин дараагийн хамгийн зөв
           цаг нь өнөөдөр юм."
         </p>
       </section>
       <section className="bg-black flex flex-col items-center py-10">
-        <h2 className="text-3xl text-white">Онцлох жиймүүд</h2>
+        <h2 className="text-lg lg:text-3xl text-white">Онцлох жиймүүд</h2>
 
         <div className="flex gap-5 justify-center py-10">
           {fitness.map((fitness: FitnessType, index: number) => (
@@ -101,7 +122,9 @@ export default function Home(props: PropsType) {
           <button className="mainButton">Бүх жийм үзэх</button>
         </Link>
       </section>
-      <Service />
+      <div id="service">
+        <Service />
+      </div>
       <section className="bg-black flex flex-col items-center py-10">
         <h2 className="text-3xl text-white">Мэдээ мэдээлэл</h2>
         <p className="text-white">
@@ -117,7 +140,6 @@ export default function Home(props: PropsType) {
         </div>
         <button className="mainButton">Бүх мэдээ үзэх</button>
       </section>
-
       <section className="bg-white text-black flex justify-center items-center py-40 gap-10">
         <div className="border-r-2 border-black h-[100px]">
           <p className="text-6xl font-bold px-10">Partners</p>
