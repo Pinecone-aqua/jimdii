96cecae14a3a1bf5dd171c387ded2455939367a0
unknown <bataa.banaa@gmail.com>
2023-05-18 18:08:01 +0800

    build error fix

--- front_end/client/pages/index.tsx
+++ front_end/client/pages/index.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
-
-// import Card from "@/components/Card";
 import Service from "@/components/Homepage/Service";
 import { FitnessType } from "@/util/types";
 import axios from "axios";
@@ -9,41 +6,44 @@ import { useCallback, useEffect, useState } from "react";
 import HomeBanner from "@/components/Homepage/HomeBanner";
 import SpecialGym from "@/components/Homepage/SpecialGym";
 import Partners from "@/components/Homepage/Partners";
+import ShowReload from "@/components/common/Reload";
 
 interface PropsType {
-  fitness: FitnessType[];
+	fitness: FitnessType[];
+	status: number;
 }
 export default function Home(props: PropsType) {
-  const { fitness } = props;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [scrollY, setScrollY] = useState(0);
+	const { fitness, status } = props;
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [scrollY, setScrollY] = useState(0);
 
-  const onScroll = useCallback(() => {
-    // const { pageYOffset, scrollY } = window;
-    // console.log("pageYoffset", pageYOffset, "scrollY", scrollY);
-    setScrollY(window.pageYOffset);
-  }, []);
+	const onScroll = useCallback(() => {
+		setScrollY(window.pageYOffset);
+	}, []);
 
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll, { passive: true });
-    // remove event on unmount to prevent a memory leak with the cleanup
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [onScroll]);
+	useEffect(() => {
+		window.addEventListener("scroll", onScroll, { passive: true });
+		return () => window.removeEventListener("scroll", onScroll);
+	}, [onScroll]);
 
-  return (
-    <main className="text-white">
-      <HomeBanner />
-      <section className="h-[80px] flex items-center text-center bg-main w-full text-sm md:h-[120px] lg:h-[160px]   sm:text-2xl  lg:text-4xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#90FFFF] via-main to-main">
-        <p className="container mx-auto w-3/4 text-white ">
-          "Эхлэх хамгийн зөв цаг нь өчигдөр байсан. Харин дараагийн хамгийн зөв
-          цаг нь өнөөдөр юм."
-        </p>
-      </section>
-      <SpecialGym fitness={fitness} />
-      <div id="service">
-        <Service />
-      </div>
-      {/* <section className="bg-black flex flex-col items-center py-10">
+	return status >= 300 ? (
+		<div className="min-h-screen bg-black text-2xl text-white flex justify-center items-center">
+			<ShowReload />
+		</div>
+	) : (
+		<main className="text-white">
+			<HomeBanner />
+			<section className="h-[80px] flex items-center text-center bg-main w-full text-sm md:h-[120px] lg:h-[160px]   sm:text-2xl  lg:text-4xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#90FFFF] via-main to-main">
+				<p className="container mx-auto w-3/4 text-white ">
+					&quot;Эхлэх хамгийн зөв цаг нь өчигдөр байсан. Харин дараагийн хамгийн
+					зөв цаг нь өнөөдөр юм. &quot;
+				</p>
+			</section>
+			<SpecialGym fitness={fitness} />
+			<div id="service">
+				<Service />
+			</div>
+			{/* <section className="bg-black flex flex-col items-center py-10">
         <h2 className="text-3xl text-white">Мэдээ мэдээлэл</h2>
         <p className="text-white">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
@@ -58,19 +58,28 @@ export default function Home(props: PropsType) {
         </div>
         <button className="mainButton">Бүх мэдээ үзэх</button>
       </section> */}
-      <Partners />
-    </main>
-  );
+			<Partners />
+		</main>
+	);
 }
 
 export async function getStaticProps() {
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}fitness/limitedFitness:limit`
-  );
-  const fitness = await res.data;
-  return {
-    props: {
-      fitness,
-    },
-  };
+	try {
+		const res = await axios.get(
+			`${process.env.NEXT_PUBLIC_BACKEND_URL}fitness/limitedFitness:limit`
+		);
+		const fitness = await res.data;
+		return {
+			props: {
+				fitness,
+				status: res.status,
+			},
+		};
+	} catch (err) {
+		return {
+			props: {
+				status: 400,
+			},
+		};
+	}
 }
--- front_end/client/pages/signUpGym.tsx
+++ front_end/client/pages/signUpGym.tsx
@@ -1,286 +1,305 @@
 import MainLogo from "@/components/subcomp/MainLogo";
-import { DiscountType, FitnessType } from "@/util/types";
+import { DiscountType } from "@/util/types";
 import axios from "axios";
 import React, { useRef, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function SignUpGym(): JSX.Element {
-  const [discounts, setDiscounts] = useState<DiscountType[]>([]);
-  const [statusMessage, setStatusMessage] = useState<number | null>(null);
-  const discountName = useRef("");
-  const discountPers = useRef("");
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async function submitHandler(e: any) {
-    e.preventDefault();
-  const [discounts, setDiscounts] = useState<DiscountType[]>([]);
-  const discountName = useRef("");
-  const discountPers = useRef("");
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  function submitHandler(e: any) {
-    e.preventDefault();
+	const [discounts, setDiscounts] = useState<DiscountType[]>([]);
+	const discountName = useRef("");
+	const discountPers = useRef("");
 
-    const newFitness = {
-      name: e.target.name.value,
-      description: e.target.description.value,
-      price: {
-        oneMonth: e.target.oneMonth.value,
-        year: e.target.year.value,
-      },
-      address: {
-        district: e.target.districts.value,
-        description: e.target.addressDescription.value,
-      },
-      timetable: {
-        Monday: e.target.workDay.value,
-        Tuesday: e.target.workDay.value,
-        Wednesday: e.target.workDay.value,
-        Thursday: e.target.workDay.value,
-        Friday: e.target.workDay.value,
-        Saturday: e.target.weekend.value,
-        Sunday: e.target.weekend.value,
-      },
-      discount: discounts,
-      spec: {
-        wifi: e.target.specWifi.value,
-        shower: e.target.specShower.value,
-        parking: e.target.specParking.value,
-      },
-      contact: {
-        // eslint-disable-next-line camelcase
-        phonenumber: e.target.phonenumber.value,
-        social: e.target.social.value,
-      },
-    };
-    console.log(newFitness);
-    const files: File[] = e.target.file.files;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	function submitHandler(e: any) {
+		e.preventDefault();
 
-    const data = new FormData();
-    Array.from(files).forEach((file) => data.append("image", file));
+		const newFitness = {
+			name: e.target.name.value,
+			description: e.target.description.value,
+			price: {
+				oneMonth: e.target.oneMonth.value,
+				year: e.target.year.value,
+			},
+			address: {
+				district: e.target.districts.value,
+				description: e.target.addressDescription.value,
+			},
+			timetable: {
+				Monday: e.target.workDay.value,
+				Tuesday: e.target.workDay.value,
+				Wednesday: e.target.workDay.value,
+				Thursday: e.target.workDay.value,
+				Friday: e.target.workDay.value,
+				Saturday: e.target.weekend.value,
+				Sunday: e.target.weekend.value,
+			},
+			discount: discounts,
+			spec: {
+				wifi: e.target.specWifi.value,
+				shower: e.target.specShower.value,
+				parking: e.target.specParking.value,
+			},
+			contact: {
+				// eslint-disable-next-line camelcase
+				phonenumber: e.target.phonenumber.value,
+				social: e.target.social.value,
+			},
+		};
+		console.log(newFitness);
+		const files: File[] = e.target.file.files;
 
-    data.append("newFitness", JSON.stringify(newFitness));
+		const data = new FormData();
+		Array.from(files).forEach((file) => data.append("image", file));
 
-    const res = await axios.post(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}tempfitness/addtempFitness`,
-      data,
-    );
-    if (res.status === 200) {
-      setStatusMessage(200);
-    } else {
-      setStatusMessage(res.status);
-    }
+		data.append("newFitness", JSON.stringify(newFitness));
 
-    // fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}fitness/addtempFitness`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(newFitness),
-    // });
+		const promise = (async () => {
+			const res = await axios.post(
+				`${process.env.NEXT_PUBLIC_BACKEND_URL}tempfitness/addtempFitness`,
+				data
+			);
+			if (res.status === 200) {
+				toast("Success", {
+					position: "bottom-left",
+					type: "success",
+					autoClose: 1000,
+					closeOnClick: true,
+					pauseOnHover: false,
+					draggable: true,
+				});
+			} else {
+				toast("Error try again", {
+					position: "bottom-left",
+					type: "error",
+					autoClose: 1000,
+					closeOnClick: true,
+					pauseOnHover: false,
+					draggable: true,
+				});
+			}
+		})();
+		promise.catch((error) => {
+			console.log(error);
+		});
+	}
 
-    // fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}fitness/addFitness`, {
-    //   headers: { "Content-Type": "application/json" },
-    //   method: "POST",
-    //   body: JSON.stringify(newFitness),
-    // });
-  }
-  function discountAdd() {
-    setDiscounts([
-      ...discounts,
-      { name: discountName.current, discount: Number(discountPers.current) },
-    ]);
-  }
-  console.log(discounts);
+	function discountAdd() {
+		setDiscounts([
+			...discounts,
+			{ name: discountName.current, discount: Number(discountPers.current) },
+		]);
+	}
 
-  return (
-    <div className="w-full h-fit registerPage">
-      <div className="w-[50%] mx-auto py-[35px]">
-        <div className="flex">
-          <div className="hidden lg:inline w-[50%] rounded-l-lg">
-            <div className="w-full h-full flex justify-center items-center bg-[#4D9799] rounded-l-lg  ">
-              <MainLogo />
-            </div>
-          </div>
+	return (
+		<div className="w-full h-fit registerPage">
+			<div className="w-[50%] mx-auto py-[35px]">
+				<div className="flex">
+					<div className="hidden lg:inline w-[50%] rounded-l-lg">
+						<div className="w-full h-full flex justify-center items-center bg-[#4D9799] rounded-l-lg  ">
+							<MainLogo />
+						</div>
+					</div>
 
-          <div className="bg-black text-white rounded-r-lg w-[50%]">
-            <div className=" text-white flex justify-center items-center h-[50px] text-lg font-bold rounded-t-lg p-[30px]">
-              <h2>Gym Registration Form</h2>
-            </div>
-            <form onSubmit={submitHandler} className="w-[80%] mx-auto">
-              <label className="block w-full">
-                <h2>НЭР :</h2>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Your Gym Name"
-                  className="w-full p-[5px]"
-                  id="input_Style"
-                  required
-                />
-              </label>
-              <label className="w-full">
-                <h2>ДЭЛГЭРЭНГҮЙ :</h2>
+					<div className="bg-black text-white rounded-r-lg w-[50%]">
+						<div className=" text-white flex justify-center items-center h-[50px] text-lg font-bold rounded-t-lg p-[30px]">
+							<h2>Gym Registration Form</h2>
+						</div>
+						<form
+							onSubmit={submitHandler}
+							className="w-[80%] mx-auto">
+							<label className="block w-full">
+								<h2>НЭР :</h2>
+								<input
+									type="text"
+									name="name"
+									placeholder="Your Gym Name"
+									className="w-full p-[5px]"
+									id="input_Style"
+									required
+								/>
+							</label>
+							<label className="w-full">
+								<h2>ДЭЛГЭРЭНГҮЙ :</h2>
 
-                <input
-                  type="text"
-                  name="description"
-                  placeholder="Description"
-                  className="w-full  p-[5px]"
-                  id="input_Style"
-                  required
-                />
-              </label>
-              <label className="w-full">
-                <h2>ҮНЭ :</h2>
+								<input
+									type="text"
+									name="description"
+									placeholder="Description"
+									className="w-full  p-[5px]"
+									id="input_Style"
+									required
+								/>
+							</label>
+							<label className="w-full">
+								<h2>ҮНЭ :</h2>
 
-                <div className="flex justify-between">
-                  <input
-                    type="text"
-                    name="oneMonth"
-                    placeholder="Month"
-                    className="w-[49%]  p-[5px]"
-                    id="input_Style"
-                    required
-                  />
-                  <input
-                    type="text"
-                    name="year"
-                    placeholder="Year"
-                    className="w-[49%]  p-[5px]"
-                    id="input_Style"
-                  />
-                </div>
-              </label>
-              <label className="block ">
-                <h2>ADDRESS :</h2>
-                <select
-                  name="districts"
-                  id="districts"
-                  className="text-white w-full bg-black border-bottom p-[5px]"
-                >
-                  <option value="Бүх дүүрэг/сум..." className="w-[50%]">
-                    Дүүрэг...
-                  </option>
-                  <option value="Bayangol">Bayangol</option>
-                  <option value="Khan-uul">Khan-uul</option>
-                  <option value="Bayanzurkh">Bayanzurkh</option>
-                  <option value="Sukhbaatar">Sukhbaatar</option>
-                  <option value="Songino-Khairkhan">Songino-Khairkhan</option>
-                  <option value="Chingeltei">Chingeltei</option>
-                </select>
-                <div className="mt-3">
-                  <h2>ХАЯГ :</h2>
-                  <input
-                    type="text"
-                    name="addressDescription"
-                    placeholder="Description"
-                    className="text-white w-full p-[5px] "
-                    id="input_Style"
-                  />
-                </div>
-              </label>
-              <label className="block">
-                <h2>ЦАГИЙН ХУВААРЬ:</h2>
+								<div className="flex justify-between">
+									<input
+										type="text"
+										name="oneMonth"
+										placeholder="Month"
+										className="w-[49%]  p-[5px]"
+										id="input_Style"
+										required
+									/>
+									<input
+										type="text"
+										name="year"
+										placeholder="Year"
+										className="w-[49%]  p-[5px]"
+										id="input_Style"
+									/>
+								</div>
+							</label>
+							<label className="block ">
+								<h2>ADDRESS :</h2>
+								<select
+									name="districts"
+									id="districts"
+									className="text-white w-full bg-black border-bottom p-[5px]">
+									<option
+										value="Бүх дүүрэг/сум..."
+										className="w-[50%]">
+										Дүүрэг...
+									</option>
+									<option value="Bayangol">Bayangol</option>
+									<option value="Khan-uul">Khan-uul</option>
+									<option value="Bayanzurkh">Bayanzurkh</option>
+									<option value="Sukhbaatar">Sukhbaatar</option>
+									<option value="Songino-Khairkhan">Songino-Khairkhan</option>
+									<option value="Chingeltei">Chingeltei</option>
+								</select>
+								<div className="mt-3">
+									<h2>ХАЯГ :</h2>
+									<input
+										type="text"
+										name="addressDescription"
+										placeholder="Description"
+										className="text-white w-full p-[5px] "
+										id="input_Style"
+									/>
+								</div>
+							</label>
+							<label className="block">
+								<h2>ЦАГИЙН ХУВААРЬ:</h2>
 
-                <div className="mt-2">
-                  <h2>Ажлын өдөр:</h2>
+								<div className="mt-2">
+									<h2>Ажлын өдөр:</h2>
 
-                  <input
-                    type="text"
-                    name="workDay"
-                    placeholder="Хэдээс хэдэн цаг хүртэл"
-                    className="text-white w-full p-[5px]"
-                    id="input_Style"
-                  />
-                </div>
-                <div>
-                  <h2>Амралтын өдөр:</h2>
+									<input
+										type="text"
+										name="workDay"
+										placeholder="Хэдээс хэдэн цаг хүртэл"
+										className="text-white w-full p-[5px]"
+										id="input_Style"
+									/>
+								</div>
+								<div>
+									<h2>Амралтын өдөр:</h2>
 
-                  <input
-                    type="text"
-                    name="weekend"
-                    placeholder="Хэдээс хэдэн цаг хүртэл"
-                    className="text-white w-full p-[5px]"
-                    id="input_Style"
-                  />
-                </div>
-              </label>
-              <label className="w-full">
-                <input type="file" name="file" multiple />
-              </label>
-              <label className="w-full">
-                <h2>ХӨНГӨЛӨЛТ :</h2>
+									<input
+										type="text"
+										name="weekend"
+										placeholder="Хэдээс хэдэн цаг хүртэл"
+										className="text-white w-full p-[5px]"
+										id="input_Style"
+									/>
+								</div>
+							</label>
+							<label className="w-full">
+								<input
+									type="file"
+									name="file"
+									multiple
+								/>
+							</label>
+							<label className="w-full">
+								<h2>ХӨНГӨЛӨЛТ :</h2>
 
-                <div>
-                  Хөнгөлөлтийн нэр:
-                  <input
-                    type="text"
-                    name="discountName"
-                    placeholder="Discount name"
-                    className="text-white w-full  p-[5px]"
-                    id="input_Style"
-                    onChange={(e) => (discountName.current = e.target.value)}
-                  />
-                </div>
-                <div>
-                  Хөнгөлөлтийн хувь:
-                  <input
-                    type="number"
-                    name="discountPer"
-                    placeholder="Discount Percentage"
-                    className="text-white w-full  p-[5px]"
-                    id="input_Style"
-                    onChange={(e) => (discountPers.current = e.target.value)}
-                  />
-                </div>
-                <input type="button" value={"+"} onClick={discountAdd} />
-              </label>
-              <label>
-                <h2>НЭМЭЛТ :</h2>
-                <div className="flex gap-[20px]">
-                  <div className="flex gap-[10px]">
-                    <h3>Wifi :</h3>
-                    <input type="checkbox" name="specWifi" />
-                  </div>
+								<div>
+									Хөнгөлөлтийн нэр:
+									<input
+										type="text"
+										name="discountName"
+										placeholder="Discount name"
+										className="text-white w-full  p-[5px]"
+										id="input_Style"
+										onChange={(e) => (discountName.current = e.target.value)}
+									/>
+								</div>
+								<div>
+									Хөнгөлөлтийн хувь:
+									<input
+										type="number"
+										name="discountPer"
+										placeholder="Discount Percentage"
+										className="text-white w-full  p-[5px]"
+										id="input_Style"
+										onChange={(e) => (discountPers.current = e.target.value)}
+									/>
+								</div>
+								<input
+									type="button"
+									value={"+"}
+									onClick={discountAdd}
+								/>
+							</label>
+							<label>
+								<h2>НЭМЭЛТ :</h2>
+								<div className="flex gap-[20px]">
+									<div className="flex gap-[10px]">
+										<h3>Wifi :</h3>
+										<input
+											type="checkbox"
+											name="specWifi"
+										/>
+									</div>
 
-                  <h3>Shower: </h3>
-                  <input type="checkbox" name="specShower" />
-                  <h3>Parking: </h3>
-                  <input type="checkbox" name="specParking" />
-                </div>
-              </label>
-              <label className="w-full">
-                <div>
-                  <h3>УТАСНЫ ДУГААР :</h3>
-                  <input
-                    type="number"
-                    name="phonenumber"
-                    placeholder="Phone Number"
-                    className="text-white w-full p-[5px]"
-                    id="input_Style"
-                  />
-                  <h3>СОШИАЛ :</h3>
-                  <input
-                    type="url"
-                    name="social"
-                    placeholder="Social Link"
-                    className="text-white w-full  p-[5px]"
-                    id="input_Style"
-                  />
-                </div>
-              </label>
-              {/* <label>
+									<h3>Shower: </h3>
+									<input
+										type="checkbox"
+										name="specShower"
+									/>
+									<h3>Parking: </h3>
+									<input
+										type="checkbox"
+										name="specParking"
+									/>
+								</div>
+							</label>
+							<label className="w-full">
+								<div>
+									<h3>УТАСНЫ ДУГААР :</h3>
+									<input
+										type="number"
+										name="phonenumber"
+										placeholder="Phone Number"
+										className="text-white w-full p-[5px]"
+										id="input_Style"
+									/>
+									<h3>СОШИАЛ :</h3>
+									<input
+										type="url"
+										name="social"
+										placeholder="Social Link"
+										className="text-white w-full  p-[5px]"
+										id="input_Style"
+									/>
+								</div>
+							</label>
+							{/* <label>
                    Image: 
                     <input type="file" name="Upload Images" multiple placeholder="Image" required/>
                 </label> */}
-              <button
-                type="submit"
-                className="bg-[#4D9799] my-[20px] p-[15px] w-full "
-              >
-                Add New Gym
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+							<button
+								type="submit"
+								className="bg-[#4D9799] my-[20px] p-[15px] w-full ">
+								Add New Gym
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
