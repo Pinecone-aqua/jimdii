ce42ffdce0d4000559c04244195e5790b0cbf022
Batsu28 <bataa.banaa@gmail.com>
2023-05-19 11:42:58 +0800

    google login test4 console log

--- backend/src/google-login/getGoogleUserInfo.ts
+++ backend/src/google-login/getGoogleUserInfo.ts
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch';
 
 export async function getGoogleUserInfo(access_token: string) {
+  console.log('google user info');
+
   const profile = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
     headers: {
       Authorization: `Bearer ${access_token}`,
